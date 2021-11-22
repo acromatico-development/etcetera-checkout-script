d80dcf4cdce8a216c9be58888d0cac2a7f109c38
Rafael González <rafa@acromatico.co>
2021-11-22 09:43:37 -0600

    Initial commit

---
+++ index.ts
@@ -0,0 +1,22 @@
+const product_property: HTMLSpanElement | null = document.querySelector(".product__description__property");
+
+console.log(product_property);
+
+if(product_property){
+    const innerText = product_property.innerText;
+    const isRecogerEnTienda = innerText.includes("Recoger");
+
+    if(isRecogerEnTienda && innerText.split(":")[1].trim() === "Sí"){
+        const orderSummary = document.querySelector(".order-summary__sections");
+        const newDiv = document.createElement("div");
+        newDiv.style.width = "100%";
+        newDiv.style.padding = "1rem";
+        newDiv.style.backgroundColor = "#434343";
+        newDiv.style.color = "#ffffff";
+        newDiv.innerHTML = `
+            <p style="font-weight: bold;">Seleccionaste Pick Up - Recoger en Tienda</p>
+        `;
+
+        orderSummary?.appendChild(newDiv);
+    }
+}
